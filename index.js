de29bcadcd61a31b5d24aedf444888e51564c0b6
Nishtha1012 <104210575+Nishtha1012@users.noreply.github.com>
2023-06-01 15:58:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const userRoutes = require('./src/routes/user.routes')
 const todoRoutes = require('./src/routes/todo.routes')
 
 const app = express()
-app.use(cors({ origin: 'https://todo-frontend-plum.vercel.app/', credentials: true }))
+app.use(cors({ origin: 'https://todo-frontend-plum.vercel.app', credentials: true }))
 app.use(express.json())
 app.use(cookieparser())
 app.use(express.urlencoded({ extended: true }));
